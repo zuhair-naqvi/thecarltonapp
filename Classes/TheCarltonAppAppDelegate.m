eb5ff058c3aad832b4fce1465ad1b5fd6e71aa8c
Zuhair Naqvi <zuhairnaqvi@Zuhair-Naqvis-MacBook-Pro.local>
2010-12-05 07:50:49 +1100

    loading defaults from settings

--- Classes/TheCarltonAppAppDelegate.m
+++ Classes/TheCarltonAppAppDelegate.m
@@ -26,7 +26,10 @@
 #pragma mark Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
-    
+
+	//[[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Root" ofType:@"plist"]]];
+	[self initSettingsDefaults];
+
 	TTNavigator* navigator = [TTNavigator navigator];
 	navigator.persistenceMode = TTNavigatorPersistenceModeNone;
 	TTURLMap* map = navigator.URLMap;
@@ -54,6 +57,7 @@
 	NSString *urlStr = [[NSString alloc] 
 						initWithFormat:@"http://%@/front_dev.php/menu/plist?seedVar=%f", server, 
 						(float)random()/RAND_MAX];
+	NSLog(@"%@", urlStr);
 	NSURL *url = [NSURL URLWithString:urlStr];
 	
 	//[[NSDictionary alloc] initWithContentsOfFile:DataPath];
@@ -156,6 +160,68 @@
 	[navigationController release];
 	[window release];
     [super dealloc];
+}
+
+/** Loads user preferences database from Settings.bundle plists. */
+- (void) initSettingsDefaults
+{
+	NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+	
+	//Determine the path to our Settings.bundle.
+	NSString *bundlePath = [[NSBundle mainBundle] bundlePath];
+	NSString *settingsBundlePath = [bundlePath stringByAppendingPathComponent:@"Settings.bundle"];
+	
+	// Load paths to all .plist files from our Settings.bundle into an array.
+	NSArray *allPlistFiles = [NSBundle pathsForResourcesOfType:@"plist" inDirectory:settingsBundlePath];
+	
+	// Put all of the keys and values into one dictionary,
+	// which we then register with the defaults.
+	NSMutableDictionary *preferencesDictionary = [NSMutableDictionary dictionary];
+	
+	// Copy the default values loaded from each plist
+	// into the system's sharedUserDefaults database.
+	NSString *plistFile;
+	for (plistFile in allPlistFiles)
+	{
+		
+		// Load our plist files to get our preferences.
+		NSDictionary *settingsDictionary = [NSDictionary dictionaryWithContentsOfFile:plistFile];
+		NSArray *preferencesArray = [settingsDictionary objectForKey:@"PreferenceSpecifiers"];
+		
+		// Iterate through the specifiers, and copy the default
+		// values into the DB.
+		NSDictionary *item;
+		for(item in preferencesArray)
+		{
+			// Obtain the specifier's key value.
+			NSString *keyValue = [item objectForKey:@"Key"];
+			
+			// Using the key, return the DefaultValue if specified in the plist.
+			// Note: We won't know the object type until after loading it.
+			id defaultValue = [item objectForKey:@"DefaultValue"];
+			
+			// Some of the items, like groups, will not have a Key, let alone
+			// a default value.  We want to safely ignore these.
+			if (keyValue && defaultValue)
+			{
+				[preferencesDictionary setObject:defaultValue forKey:keyValue];
+			}
+			
+		}
+		
+	}
+	
+	// Ensure the version number is up-to-date, too.
+	// This is, incidentally, how you update the value in a Title element.
+	NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+	NSString *shortVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+	NSString *versionLabel = [NSString stringWithFormat:@"%@ (%d)", shortVersion, [version intValue]];
+	[standardUserDefaults setObject:versionLabel forKey:@"app_version_number"];
+	
+	// Now synchronize the user defaults DB in memory
+	// with the persistent copy on disk.
+	[standardUserDefaults registerDefaults:preferencesDictionary];
+	[standardUserDefaults synchronize];
 }
 
 
